c8cc79bcd4afb97141cc52e91e6d8ed56b8250c0
Yusuke Hata <yusuke.hata@fillmoreadvisory.jp>
2012-01-19 12:14:23 +0900

    chat.js

--- example-nodejs-server/chat.js
+++ example-nodejs-server/chat.js
@@ -1,11 +1,19 @@
 var io = require('socket.io').listen(8080);
+
+var c = [];
 io.sockets.on('connection', function(client){
   console.log('connect: ' + client.id);
+  client.broadcast.emit('user connected');
+  c.push(client);
 
   client.on('message', function (message){
     console.log('client message: ' + message);
     client.send('rely:' + message);
     client.broadcast.emit(client.id + ' says: ' + message);
+    // emulate broadcast
+    c.forEach(function(e){
+      e.send(' says: ' + message);
+    });
   });
 
   client.on('disconnect', function(){
